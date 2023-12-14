9887cb07f46d7db3ddb20a46c97c70a486d5dc6b
VenkateswarluJakki <venkyj339@gmail.com>
2023-12-14 13:49:01 +0530

    upload my data

--- src/maven/Mian.java
+++ src/maven/Mian.java
@@ -4,16 +4,14 @@ public class Mian {
 	
 	private int id;
    private  String name;
-public int getId() {
-	return id;
-}
-public void setId(int id) {
+   
+
+
+   public void setId(int id) {
 	this.id = id;
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
+ }
+
+  public void setName(String name) {
 	this.name = name;
 }
    
